15fa50897328445001faf5abdf46f5ecde513a4b
Ellie Parry <ellierparry@gmail.com>
2024-07-24 13:20:26 +0100

    deleted disable powered by x.

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Use Helmet as the first middleware
 app.use(helmet());
-
-// Explicitly disable 'X-Powered-By' header
-app.disable('x-powered-by');
 
 // Define a simple route
 app.get('/', (req, res) => {
@@ -18,3 +15,23 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+fetch('http://localhost:3000')
+  .then(response => {
+    // Access the headers
+    for (let pair of response.headers.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+
+    // Process the response body if needed
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+  
