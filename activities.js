10a81c5a8acbecee9560289ddb3a56e0e7c4edae
Ellie Parry <ellierparry@gmail.com>
2024-07-25 11:59:56 +0100

    Added uuidv4 imported.

---
+++ activities.js
@@ -0,0 +1,21 @@
+export const activities = [
+    {
+      "id": "54321234",
+      "activity_submitted": Date.now().toString(),
+      "activity_type": "run",
+      "activity_duration": "30"
+    },
+    {
+      "id": "12345678",
+      "activity_submitted": Date.now().toString(),
+      "activity_type": "walk",
+      "activity_duration": "45"
+    },
+    {
+      "id": "87654321",
+      "activity_submitted": Date.now().toString(),
+      "activity_type": "bike",
+      "activity_duration": "60"
+    }
+  ];
+
